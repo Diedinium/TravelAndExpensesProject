712d6c86988d59e719aa12a99ed34f2aea89b10e
Jake Hall <jakethomashall@gmail.com>
2019-10-14 21:15:12 +0100

    Add project files.

---
+++ TravelAndExpenses/TravelAndExpenses.cpp
@@ -0,0 +1,15 @@
+// TravelAndExpenses.cpp : The main application file for the travel and expenses console app.
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <numeric>
+#include <sstream>
+#include <vector>
+
+int main()
+{
+    
+
+	return 0;
+}
