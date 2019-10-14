049d94e16d6b6cbf5ac8ac28fe9eb7ad5a95ce4f
Jake Hall <jakethomashall@gmail.com>
2019-10-14 23:04:19 +0100

    Created struct and enum for storing journey details.

--- TravelAndExpenses/TravelAndExpenses.cpp
+++ TravelAndExpenses/TravelAndExpenses.cpp
@@ -6,10 +6,74 @@
 #include <numeric>
 #include <sstream>
 #include <vector>
+#include <iomanip>
+#include <map>
+
+enum class TravelType
+{
+	Travel,
+	TravelAndExpense
+};
+
+struct Journey {
+	// Struct variables
+	TravelType travelType;
+	double travelCost;
+	double expenseCost;
+	double totalCost;
+	double taxReclaim;
+
+	// Constructor for travel only
+	Journey(TravelType passTravelType, double passCost)
+	{
+		travelType = passTravelType;
+		travelCost = passCost;
+		totalCost = travelCost;
+		expenseCost = 0;
+		taxReclaim = totalCost * 0.2;
+	}
+	// Constructor used when there is travel and expenses
+	Journey(TravelType passTravelType, double passTravelCost, double passExpenseCost) {
+		travelType = passTravelType;
+		travelCost = passTravelCost;
+		expenseCost = passExpenseCost;
+		totalCost = travelCost + expenseCost;
+		taxReclaim = totalCost * 0.2;
+	}
+
+
+};
+
+std::vector<Journey> vecJourneyCollection;
 
 int main()
 {
-    
+	
+
+	Journey journey1{ TravelType::Travel, 23.23 };
+	Journey journey2{ TravelType::TravelAndExpense, 23.23, 10.92 };
+	Journey journey3{ TravelType::Travel, 25.30 };
+	
+	vecJourneyCollection.push_back(journey1);
+	vecJourneyCollection.push_back(journey2);
+	vecJourneyCollection.push_back(journey3);
+
+	for (int i = 0; i < (int)vecJourneyCollection.size(); ++i) {
+		if (vecJourneyCollection[i].travelType == TravelType::Travel) {
+			std::cout << "Travel type : " << ((((int)vecJourneyCollection[i].travelType) == 0) ? "Travel" : "Travel and Expenses")<< "\n";
+			std::cout << "Total Cost : \x9C"  << vecJourneyCollection[i].totalCost << "\n";
+			printf("Tax Reclaimable : \x9C%.2f\n", vecJourneyCollection[i].taxReclaim);
+			std::cout << " " << "\n";
+		}
+		else {
+			std::cout << "Travel type : " << ((((int)vecJourneyCollection[i].travelType) == 0) ? "Travel" : "Travel and Expenses") << "\n";
+			std::cout << "Travel Cost : \x9C" << vecJourneyCollection[i].travelCost << "\n";
+			std::cout << "Expense Cost : \x9C" << vecJourneyCollection[i].expenseCost << "\n";
+			std::cout << "Total Cost : \x9C" << vecJourneyCollection[i].totalCost << "\n";
+			printf("Tax Reclaimable : \x9C%.2f\n", vecJourneyCollection[i].taxReclaim);
+			std::cout << " " << "\n";
+		}
+	}
 
 	return 0;
 }
