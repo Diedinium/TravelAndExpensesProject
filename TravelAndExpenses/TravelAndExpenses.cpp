93617eb99ba2526b10ae87657a315be68e500f79
Jake Hall <jake-thomas-hall@hotmail.co.uk>
2019-10-16 15:05:51 +0100

    Started formatting output so that it presents as a table.

--- TravelAndExpenses/TravelAndExpenses.cpp
+++ TravelAndExpenses/TravelAndExpenses.cpp
@@ -59,27 +59,36 @@ std::vector<Journey> vecJourneyCollection;
 
 int main()
 {
-	
+	int intWidth = 15;
 
 	Journey journey1{ TravelType::Travel, 25.23 };
 	Journey journey2{ TravelType::TravelAndExpense, 23.23, 10.92 };
 	Journey journey3{ TravelType::Travel, 25.23 };
 	Journey journey4{ TravelType::TravelAndExpense, 120.25, 57.82 };
-	
+
 	vecJourneyCollection.push_back(journey1);
 	vecJourneyCollection.push_back(journey2);
 	vecJourneyCollection.push_back(journey3);
 	vecJourneyCollection.push_back(journey4);
 
+	std::cout << "Travel Only Journeys" << "\n";
+	std::cout << "--------------------------------------------------------------------\n";
+	std::cout << std::setw(intWidth) << "Travel Type" << " |" << std::setw(intWidth) << "Travel Cost" << " |" << std::setw(intWidth) << "Tax Reclaim" << " |" << std::setw(intWidth) << "Final Payment" << " |" << std::endl;
+	std::cout << "--------------------------------------------------------------------\n";
 	for (int i = 0; i < (int)vecJourneyCollection.size(); ++i) {
 		if (vecJourneyCollection[i].travelType == TravelType::Travel) {
-			std::cout << "Travel type : " << ((((int)vecJourneyCollection[i].travelType) == 0) ? "Travel" : "Travel and Expenses")<< "\n";
-			std::cout << "Total Cost : \x9C"  << vecJourneyCollection[i].totalCost << "\n";
-			printf("Tax Reclaimable : \x9C%.2f\n", vecJourneyCollection[i].taxReclaim);
-			printf("Final payment made : \x9C%.2f\n", vecJourneyCollection[i].finalPayment);
-			std::cout << " " << "\n";
+			std::cout << std::setw(intWidth) << ((((int)vecJourneyCollection[i].travelType) == 0) ? "Travel" : "Travel and Expenses")
+			<< std::setw(intWidth) << vecJourneyCollection[i].totalCost
+			<< std::setw(intWidth) << vecJourneyCollection[i].taxReclaim
+			<< std::setw(intWidth) << vecJourneyCollection[i].finalPayment << "\n";
 		}
-		else {
+	}
+	std::cout << "\n";
+
+	std::cout << "Travel and Expenses Journeys\n";
+	std::cout << "--------------------------------------------------------------------\n";
+	for (int i = 0; i < (int)vecJourneyCollection.size(); ++i) {
+		if (vecJourneyCollection[i].travelType == TravelType::TravelAndExpense) {
 			std::cout << "Travel type : " << ((((int)vecJourneyCollection[i].travelType) == 0) ? "Travel" : "Travel and Expenses") << "\n";
 			std::cout << "Travel Cost : \x9C" << vecJourneyCollection[i].travelCost << "\n";
 			std::cout << "Expense Cost : \x9C" << vecJourneyCollection[i].expenseCost << "\n";
