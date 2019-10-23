1dbe5a7fede9fafc93367afc799352157c34576e
Jake-Desktop <jakethomashall@gmail.com>
2019-10-23 23:02:07 +0100

    Pass by pointer fix implemented

--- TravelAndExpenses/TravelAndExpenses.cpp
+++ TravelAndExpenses/TravelAndExpenses.cpp
@@ -1,4 +1,6 @@
 // TravelAndExpenses.cpp : The main application file for the travel and expenses console app.
+// Please note: This file has XML style comments to enable intellisense, this is not a standard feature of C++! You will need an extension called "CppTripleSlash - xml doc comments for c++"
+// installed in order for this functionality to work, this not being installed does not affect functionality, it simply results in less descriptive function descriptions.
 
 #include <iostream>
 #include <string>
@@ -56,96 +58,82 @@ struct Journey {
 		}
 		finalPayment = expensePayable + travelCost;
 	}
-
-
 };
-
-// Global Variables
-
-std::vector<Journey> vecJourneyCollection;
-const int intWidth = 15;
-
-// Global Variables end
 
 // Function namespace declaration
 
 // Ouput print help functions.
 void PrintIntro();
-void PrintTravelHeader();
-void PrintTravelExpenseHeader();
-void PrintCurrentJourneysTravel();
-void PrintCurrentJourneysTravelExpense();
-void PrintAddJourneyScreen();
-void PrintMainMenuScreen();
+void PrintTravelHeader(int* intWidth);
+void PrintTravelExpenseHeader(int* intWidth);
+void PrintCurrentJourneysTravel(std::vector<Journey>* vecJourneyCollection, int* intWidth);
+void PrintCurrentJourneysTravelExpense(std::vector<Journey>* vecJourneyCollection, int* intWidth);
+void PrintAddJourneyScreen(std::vector<Journey>* vecJourneyCollection);
+void PrintMainMenuScreen(std::vector<Journey>* vecJourneyCollection);
 void PrintSummaryScreen();
 
 // Misc helper functions
 void Pause();
-void TestingFunction();
-int ValidateChoiceInput();
+int ValidateIntInput();
+double ValidateDoubleInput();
 
-void TestFunction(std::vector<Journey> *vecJourneyCollectionPass);
+void TestFunction(std::vector<Journey> *vecJourneyCollection);
 
 // Menu action functinons
-void AddNewJourney();
-void ViewJourneys();
+void AddNewJourney(std::vector<Journey>* vecJourneyCollection, int* intWidth);
+void ViewJourneys(std::vector<Journey>* vecJourneyCollection, int* intWidth);
 void ViewSummary();
 
 //Function namespace declaration end
 
 int main()
 {
-	std::vector<Journey> vecJourneyCollection2;
-	TestFunction(&vecJourneyCollection2);
+	int intWidth = 15;
+	std::vector<Journey> vecJourneyCollection;
 
 	PrintIntro();
 
 	int intMenuMainChoice = 0;
 	bool boolExitWhile = false;
 
-	TestingFunction();
+	TestFunction(&vecJourneyCollection);
 
 	do {
-		PrintMainMenuScreen();
+		PrintMainMenuScreen(&vecJourneyCollection);
 
 		while (boolExitWhile == false) {
 			std::cout << "Enter choice : ";
-			intMenuMainChoice = ValidateChoiceInput();
+			intMenuMainChoice = ValidateIntInput();
 
 			try {
 				switch (intMenuMainChoice)
 				{
 				case 1: {
-					AddNewJourney();
+					AddNewJourney(&vecJourneyCollection, &intWidth);
 					boolExitWhile = true;
-					intMenuMainChoice = 0;
 					break;
 				}
 				case 2: {
 					if (vecJourneyCollection.size() <= 0) {
 						boolExitWhile = false;
-						intMenuMainChoice = 0;
 						throw std::runtime_error("You have not yet added any journeys, this menu option is not available yet.");
 						break;
 					}
 					else {
-						ViewJourneys();
+						ViewJourneys(&vecJourneyCollection, &intWidth);
 						boolExitWhile = true;
-						intMenuMainChoice = 0;
 					}
 					break;
 				}
 				case 3: {
 					if (vecJourneyCollection.size() <= 0) {
 						boolExitWhile = false;
-						intMenuMainChoice = 0;
 						throw std::runtime_error("You have not yet added any journeys, this menu option is not available yet.");
 						break;
 					}
 					else {
 						ViewSummary();
 						boolExitWhile = true;
-						intMenuMainChoice = 0;
 					}
 					break;
 				}
@@ -156,7 +144,7 @@ int main()
 				}
 				default:
 					boolExitWhile = false;
-					intMenuMainChoice = 0;
+
 					throw std::runtime_error("Input not recongised as a valid option.");
 					break;
 				}
@@ -164,7 +152,6 @@ int main()
 			catch (std::exception & exp) {
 				std::cout << "Error : " << exp.what() << "\n";
 				boolExitWhile = false;
-				intMenuMainChoice = 0;
 			}
 
 		}
@@ -178,7 +165,7 @@ int main()
 
 // Functions
 
-void AddNewJourney() {
+void AddNewJourney(std::vector<Journey>* vecJourneyCollection, int* intWidth) {
 	std::string strContinueAddingJourneys;
 	bool boolExitWhile = false;
 
@@ -189,65 +176,45 @@ void AddNewJourney() {
 	double dExpenseCost = 0;
 
 	do {
-		PrintAddJourneyScreen();
+		PrintAddJourneyScreen( vecJourneyCollection );
 
 		while (boolExitWhile != true) {
 			std::cout << "Enter choice  : ";
 
-			while (!(std::cin >> intTravelChoiceInput)) {
-				// Makes user try again if input is not an int
-				std::cout << "ERROR: a whole number must be entered : ";
-				std::cin.clear();
-				std::cin.ignore(123, '\n');
-			}
+			intTravelChoiceInput = ValidateIntInput();
 
 			try {
 				if (intTravelChoiceInput == 1) {
 					travelTypeChoice = TravelType::Travel;
 					std::cout << "Travel cost : ";
 
-					while (!(std::cin >> dTravelCost)) {
-						// Makes user try again if input is not an int
-						std::cout << "ERROR: a number must be entered : ";
-						std::cin.clear();
-						std::cin.ignore(123, '\n');
-					}
+					dTravelCost = ValidateDoubleInput();
 
 					Journey newJourney{ travelTypeChoice, dTravelCost };
-					vecJourneyCollection.push_back(newJourney);
+					vecJourneyCollection->push_back(newJourney);
 					boolExitWhile = true;
 				}
 				else if (intTravelChoiceInput == 2) {
 					travelTypeChoice = TravelType::TravelAndExpense;
 					std::cout << "Travel cost : ";
 
-					while (!(std::cin >> dTravelCost)) {
-						// Makes user try again if input is not an int
-						std::cout << "ERROR: a number must be entered : ";
-						std::cin.clear();
-						std::cin.ignore(123, '\n');
-					}
+					dTravelCost = ValidateDoubleInput();
 
 					std::cout << "Expenses cost : ";
 
-					while (!(std::cin >> dExpenseCost)) {
-						// Makes user try again if input is not an int
-						std::cout << "ERROR: a number must be entered : ";
-						std::cin.clear();
-						std::cin.ignore(123, '\n');
-					}
+					dExpenseCost = ValidateDoubleInput();
 
 					Journey newJourney{ travelTypeChoice, dTravelCost, dExpenseCost };
-					vecJourneyCollection.push_back(newJourney);
+					vecJourneyCollection->push_back(newJourney);
 					boolExitWhile = true;
 				}
 				else if (intTravelChoiceInput == 3) {
-					if (vecJourneyCollection.size() <= 0) {
+					if (vecJourneyCollection->size() <= 0) {
 						throw std::runtime_error("This option is not yet a valid option, there are no stored journeys.");
 						boolExitWhile = false;
 					}
 					else {
-						ViewJourneys();
+						ViewJourneys(vecJourneyCollection, intWidth);
 						boolExitWhile = true;
 					}
 				}
@@ -299,13 +266,13 @@ void AddNewJourney() {
 	strContinueAddingJourneys = "no";
 }
 
-void ViewJourneys() {
+void ViewJourneys(std::vector<Journey>* vecJourneyCollection, int* intWidth) {
 	system("CLS");
-	PrintTravelHeader();
-	PrintCurrentJourneysTravel();
+	PrintTravelHeader(intWidth);
+	PrintCurrentJourneysTravel(vecJourneyCollection, intWidth);
 
-	PrintTravelExpenseHeader();
-	PrintCurrentJourneysTravelExpense();
+	PrintTravelExpenseHeader(intWidth);
+	PrintCurrentJourneysTravelExpense(vecJourneyCollection, intWidth);
 	Pause();
 }
 
@@ -318,7 +285,7 @@ void ViewSummary() {
 
 		while (boolExitWhile == false) {
 			std::cout << "Enter choice : ";
-			intSummaryChoiceInput = ValidateChoiceInput();
+			intSummaryChoiceInput = ValidateIntInput();
 
 			// TODO : Implement functionality that allows summaries of items
 			try {
@@ -362,33 +329,66 @@ void ViewSummary() {
 
 }
 
-void PrintTravelHeader() {
+void PrintTravelHeader(int* intWidth) {
 	std::cout << "Travel Only Journeys" << "\n";
 	std::cout << "------------------------------------------------------------\n";
-	std::cout << std::setw(intWidth) << "Travel Type" << std::setw(intWidth) << "Travel Cost" << std::setw(intWidth) << "Tax Reclaim" << std::setw(intWidth) << "Final Pay" << "\n";
+	std::cout << std::setw(*intWidth) << "Travel Type" << std::setw(*intWidth) << "Travel Cost" << std::setw(*intWidth) << "Tax Reclaim" << std::setw(*intWidth) << "Final Pay" << "\n";
 	std::cout << "------------------------------------------------------------\n";
 }
 
-void PrintTravelExpenseHeader() {
+void PrintTravelExpenseHeader(int* intWidth) {
 	std::cout << "Travel and Expenses Journeys" << "\n";
 	std::cout << "------------------------------------------------------------------------------------------------------------------------\n";
-	std::cout << std::setw(intWidth) << "Travel Type" << std::setw(intWidth) << "Travel Cost" << std::setw(intWidth) << "Exp Cost" << std::setw(intWidth) << "Total Cost" << std::setw(intWidth) << "Exp Payable" << std::setw(intWidth) << "Tax Reclaim" << std::setw(intWidth) << "Exp not Cvrd" << std::setw(intWidth) << "Final Pay\n";
+	std::cout << std::setw(*intWidth) << "Travel Type" << std::setw(*intWidth) << "Travel Cost" << std::setw(*intWidth) << "Exp Cost" << std::setw(*intWidth) << "Total Cost" << std::setw(*intWidth) << "Exp Payable" << std::setw(*intWidth) << "Tax Reclaim" << std::setw(*intWidth) << "Exp not Cvrd" << std::setw(*intWidth) << "Final Pay\n";
 	std::cout << "------------------------------------------------------------------------------------------------------------------------\n";
 }
 
-void PrintCurrentJourneysTravel() {
-	if (vecJourneyCollection.size() <= 0) {
+/// <summary>
+/// Prints the currently stored Journeys that are of the Travel type from the Journey vector.
+/// </summary>
+/// <param name="vecJourneyCollection"></param>
+/// <param name="intWidth"></param>
+void PrintCurrentJourneysTravel(std::vector<Journey>* vecJourneyCollection, int* intWidth) {
+	if (vecJourneyCollection->size() <= 0) {
 		std::cout << "There are currently no stored journeys\n";
 	}
 	else {
-		for (int i = 0; i < (int)vecJourneyCollection.size(); ++i) {
-			if (vecJourneyCollection[i].travelType == TravelType::Travel) {
+		for (std::size_t i = 0; i < vecJourneyCollection->size(); ++i) {
+			if (vecJourneyCollection->at(i).travelType == TravelType::Travel) {
 				std::cout.precision(2);
 				std::cout
-					<< std::setw(intWidth) << ((((int)vecJourneyCollection[i].travelType) == 0) ? "Travel" : "Travel & Exp")
-					<< std::setw(intWidth) << std::fixed << vecJourneyCollection[i].totalCost
-					<< std::setw(intWidth) << std::fixed << vecJourneyCollection[i].taxReclaim
-					<< std::setw(intWidth) << std::fixed << vecJourneyCollection[i].finalPayment << "\n";
+					<< std::setw(*intWidth) << ((((int)vecJourneyCollection->at(i).travelType) == 0) ? "Travel" : "Travel & Exp")
+					<< std::setw(*intWidth) << std::fixed << vecJourneyCollection->at(i).taxReclaim
+					<< std::setw(*intWidth) << std::fixed << vecJourneyCollection->at(i).totalCost
+					<< std::setw(*intWidth) << std::fixed << vecJourneyCollection->at(i).finalPayment << "\n";
+			}
+		}
+		std::cout << "\n";
+	}
+}
+
+/// <summary>
+/// Prints the currently stored Journeys that are of the TravelAndExpense type from the Journey vector.
+/// </summary>
+/// <param name="vecJourneyCollection"></param>
+/// <param name="intWidth"></param>
+void PrintCurrentJourneysTravelExpense(std::vector<Journey>* vecJourneyCollection, int* intWidth) {
+	if (vecJourneyCollection->size() <= 0) {
+		std::cout << "There are currently no stored journeys\n";
+	}
+	else {
+		for (std::size_t i = 0; i < vecJourneyCollection->size(); ++i) {
+			if (vecJourneyCollection->at(i).travelType == TravelType::TravelAndExpense) {
+				std::cout.precision(2);
+				std::cout
+					<< std::setw(*intWidth) << ((((int)vecJourneyCollection->at(i).travelType) == 0) ? "Travel" : "Travel & Exp")
+					<< std::fixed << std::setw(*intWidth) << vecJourneyCollection->at(i).travelCost
+					<< std::fixed << std::setw(*intWidth) << vecJourneyCollection->at(i).expenseCost
+					<< std::fixed << std::setw(*intWidth) << vecJourneyCollection->at(i).totalCost
+					<< std::fixed << std::setw(*intWidth) << vecJourneyCollection->at(i).expensePayable
+					<< std::fixed << std::setw(*intWidth) << vecJourneyCollection->at(i).taxReclaim
+					<< std::fixed << std::setw(*intWidth) << vecJourneyCollection->at(i).expenseNotCovered
+					<< std::fixed << std::setw(*intWidth) << vecJourneyCollection->at(i).finalPayment << "\n";
 			}
 		}
 		std::cout << "\n";
@@ -396,30 +396,9 @@ void PrintCurrentJourneysTravel() {
 
 }
 
-void PrintCurrentJourneysTravelExpense() {
-	if (vecJourneyCollection.size() <= 0) {
-		std::cout << "There are currently no stored journeys\n";
-	}
-	else {
-		for (int i = 0; i < (int)vecJourneyCollection.size(); ++i) {
-			if (vecJourneyCollection[i].travelType == TravelType::TravelAndExpense) {
-				std::cout.precision(2);
-				std::cout
-					<< std::setw(intWidth) << ((((int)vecJourneyCollection[i].travelType) == 0) ? "Travel" : "Travel & Exp")
-					<< std::fixed << std::setw(intWidth) << vecJourneyCollection[i].travelCost
-					<< std::fixed << std::setw(intWidth) << vecJourneyCollection[i].expenseCost
-					<< std::fixed << std::setw(intWidth) << vecJourneyCollection[i].totalCost
-					<< std::fixed << std::setw(intWidth) << vecJourneyCollection[i].expensePayable
-					<< std::fixed << std::setw(intWidth) << vecJourneyCollection[i].taxReclaim
-					<< std::fixed << std::setw(intWidth) << vecJourneyCollection[i].expenseNotCovered
-					<< std::fixed << std::setw(intWidth) << vecJourneyCollection[i].finalPayment << "\n";
-			}
-		}
-		std::cout << "\n";
-	}
-
-}
-
+/// <summary>
+/// Prints the intro warning/notice when loading the program.
+/// </summary>
 void PrintIntro() {
 	// Notice for users about setup needed to ensure proper formatted output.
 	std::cout << "Notice: To ensure proper text formatting while in use, please make sure you right click on the console window header, \n";
@@ -429,7 +408,7 @@ void PrintIntro() {
 	Pause();
 }
 
-void PrintMainMenuScreen() {
+void PrintMainMenuScreen(std::vector<Journey>* vecJourneyCollection) {
 	system("cls");
 	std::cout << "-------------------------------------------\n";
 	std::cout << "\n";
@@ -437,10 +416,10 @@ void PrintMainMenuScreen() {
 	std::cout << "Please specify your option:\n";
 	std::cout << "\n";
 	std::cout << "Option 1: Enter a new Journey\n";
-	if (vecJourneyCollection.size() > 0) {
+	if (vecJourneyCollection->size() > 0) {
 		std::cout << "Option 2: View current journeys\n";
 	}
-	if (vecJourneyCollection.size() > 0) {
+	if (vecJourneyCollection->size() > 0) {
 		std::cout << "Option 3: Summaries\n";
 	}
 	std::cout << "Option 9: Exit\n";
@@ -449,7 +428,7 @@ void PrintMainMenuScreen() {
 	std::cout << "\n";
 }
 
-void PrintAddJourneyScreen() {
+void PrintAddJourneyScreen(std::vector<Journey>* vecJourneyCollection) {
 	system("cls");
 	std::cout << "-------------------------------------------\n";
 	std::cout << "\n";
@@ -458,7 +437,7 @@ void PrintAddJourneyScreen() {
 	std::cout << "\n";
 	std::cout << "Option 1 : Travel only\n";
 	std::cout << "Option 2 : Travel and expenses\n";
-	if (vecJourneyCollection.size() > 0) {
+	if (vecJourneyCollection->size() > 0) {
 		std::cout << "Option 3 : View current journeys\n";
 	}
 	std::cout << "Option 4 : Return to menu\n";
@@ -484,7 +463,7 @@ void PrintSummaryScreen() {
 	std::cout << "\n";
 }
 
-int ValidateChoiceInput() {
+int ValidateIntInput() {
 	int ChoiceValue;
 	while (!(std::cin >> ChoiceValue)) {
 		// Makes user try again if input is not an int
@@ -496,6 +475,18 @@ int ValidateChoiceInput() {
 	return ChoiceValue;
 }
 
+double ValidateDoubleInput() {
+	double DoubleValue;
+	while (!(std::cin >> DoubleValue)) {
+		// Makes user try again if input is not an int
+		std::cout << "ERROR: a decimal number must be entered : ";
+		std::cin.clear();
+		std::cin.ignore(123, '\n');
+	}
+
+	return DoubleValue;
+}
+
 void Pause()
 {
 	std::cout << "\n";
@@ -503,7 +494,7 @@ void Pause()
 	_getch();
 }
 
-void TestingFunction() {
+void TestFunction(std::vector<Journey>* vecJourneyCollection) {
 	// Testing function, adds some example journeys with large decimal point inputs.
 
 	Journey journey1{ TravelType::Travel, 25.23534 };
@@ -511,20 +502,10 @@ void TestingFunction() {
 	Journey journey3{ TravelType::Travel, 27 };
 	Journey journey4{ TravelType::TravelAndExpense, 120.2553, 57.823 };
 
-	vecJourneyCollection.push_back(journey1);
-	vecJourneyCollection.push_back(journey2);
-	vecJourneyCollection.push_back(journey3);
-	vecJourneyCollection.push_back(journey4);
-}
-
-// Experiemnt with passing by reference, needs implementing!
-void TestFunction(std::vector<Journey>* vecJourneyCollectionPass) {
-	double tempVar;
-	Journey tempJourney{ TravelType::TravelAndExpense, 25, 50 };
-	vecJourneyCollectionPass->push_back(tempJourney);
-	tempVar = vecJourneyCollectionPass->at(0).expenseCost;
-	std::cout << "Temp var is : " << tempVar;
-	Pause();
+	vecJourneyCollection->push_back(journey1);
+	vecJourneyCollection->push_back(journey2);
+	vecJourneyCollection->push_back(journey3);
+	vecJourneyCollection->push_back(journey4);
 }
 
 // Functions end
